a2867c1fffdc54bdc4be11e6ab3a7d7dc3da43cb
Rafal <Rafi565@interia.pl>
2025-03-24 15:22:45 +0100

    Naprawilem menu

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // Hamburger menu
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('nav-links');
-
+const offScreenMenu = document.querySelector(".off-screen-menu");
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('open');
+  
 });
 
 // Prosta rotacja obrazków po prawej stronie w sekcji intro
