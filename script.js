ca9f515747fb0c67a532d7d7409e04d575885167
Rafal <Rafi565@interia.pl>
2025-03-24 14:21:11 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// Hamburger menu
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('open');
+});
+
+// Prosta rotacja obrazków po prawej stronie w sekcji intro
+const introImage = document.getElementById('introImage');
+
+// Tablica z adresami (URL) kolejnych grafik.
+// Możesz wstawić tu swoje obrazy lub linki do zewnętrznych zasobów.
+const images = [
+  "images/projekt1.jpg",
+  "images/projekt2.png",
+  "images/projekt3.jpg"
+];
+
+let currentIndex = 0;
+
+// Funkcja zmienia obraz co 3 sekundy
+function changeImage() {
+  currentIndex = (currentIndex + 1) % images.length;
+  introImage.src = images[currentIndex];
+}
+
+// Uruchamiamy interwał
+setInterval(changeImage, 3000);
