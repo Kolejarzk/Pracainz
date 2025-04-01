94a3504bc7d6f09491756e880a04c3eebf563680
Rafal <Rafi565@interia.pl>
2025-04-01 20:11:37 +0200

    Animacja przejscia stron

--- script.js
+++ script.js
@@ -35,24 +35,65 @@ setInterval(changeImage, 3000);
 
 
 
-
-document.addEventListener('DOMContentLoaded', function() {
+/*
+document.addEventListener("DOMContentLoaded", function () {
   barba.init({
     transitions: [{
-      name: 'opacity-transition',
+      name: "slide",
       leave(data) {
-        return gsap.to(data.current.container, {
-          opacity: 0
-        });
+        return gsap.to(data.current.container, { x: "-100%", opacity: 0, duration: 0.9 });
       },
       enter(data) {
-        return gsap.from(data.next.container, {
-          opacity: 0
-        });
+        return gsap.from(data.next.container, { x: "100%", opacity: 0, duration: 0.5 });
       }
     }]
   });
 });
+*/
+
+function pageTransition() {
+  var tl =gsap.timeline();
+  tl.to('ul.transition li', {duration: .5, scaleY:1,transformOrigin:"bottom left", stagger:.2 })
+  tl.to('ul.transition li', {duration: .5,scaleY: 0, transformOrigin:"bottom left", stagger: .1, delay: .1 })
+}
+
+ function delay(n) {
+  n = n || 2000;
+  return new Promise(done => {
+    setTimeout(()=> {
+      done();
+    },n);
+  }); 
+ }
+
+ function contentAnimation() {
+
+ }
+
+
+  barba.init({
+       async: true, 
+        
+       
+          transitions: [{
+            
+            async leave(data) {
+              const done = this.async();
+               
+
+               pageTransition();
+               await delay(1500);
+               done(); 
+            },
+            async enter(data) {
+              contentAnimation();
+            },
+            async once(data){
+             contentAnimation();
+            }
+    }]
+  })
+
 
 
 
